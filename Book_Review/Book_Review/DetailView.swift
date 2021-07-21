4dcbee64039d658c5855e9d79789969ec794d6cf
Loi Pham <loitpham@gmail.com>
2021-07-20 21:43:27 -0700

    Using an alert to pop a NavigationLink programmatically.

--- Book_Review/Book_Review/DetailView.swift
+++ Book_Review/Book_Review/DetailView.swift
@@ -9,6 +9,10 @@ import CoreData
 import SwiftUI
 
 struct DetailView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    @State private var showingDeleteAlert = false
+    
     let book: Book
     
     var body: some View {
@@ -40,6 +44,24 @@ struct DetailView: View {
             }
         }
         .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
+        .alert(isPresented: $showingDeleteAlert) {
+            Alert(title: Text("Delete book"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete")) {
+                deleteBook()
+            }, secondaryButton: .cancel())
+        }
+        .navigationBarItems(trailing: Button(action: {
+            showingDeleteAlert = true
+        }) {
+            Image(systemName: "trash")
+        })
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        
+        // try? moc.save()
+        
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
